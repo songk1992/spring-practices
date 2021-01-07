51fa88db3aa5c44694e086038fbb850275c43b29
shk <shk@DESKTOP-C27PVRS>
2021-01-07 11:02:10 +0900

    init aoptest

---
+++ aoptest/src/main/java/com/bitacademy/aoptest/service/ProductService.java
@@ -0,0 +1,16 @@
+
+package com.bitacademy.aoptest.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bitacademy.aoptest.vo.ProductVo;
+
+@Service
+public class ProductService {
+
+	public ProductVo find(String name) {
+		System.out.println("[ProductService] finding...");
+		return new ProductVo(name);
+	}
+
+}
