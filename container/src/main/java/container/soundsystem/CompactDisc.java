7b8b3422e29c91529df44110f39f676337bf8215
shk <shk@DESKTOP-C27PVRS>
2021-01-14 10:50:57 +0900

    soundsystem #2 JUNIT TEST

---
+++ container/src/main/java/container/soundsystem/CompactDisc.java
@@ -0,0 +1,7 @@
+package container.soundsystem;
+
+public interface CompactDisc {
+
+	void play();
+
+}
---
+++ container/src/test/java/container/soundsystem/CDPlayerXMLConfigTest.java
@@ -0,0 +1,36 @@
+package container.soundsystem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes={AppConfig01.class})
+@ContextConfiguration(locations={"/container/config/soundsystem/CDPlayerConfig.xml"})
+public class CDPlayerXMLConfigTest {
+
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+	
+	@Autowired
+	private CDPlayer cdPlayer;
+	
+	@Test
+	public void testCDPlayerNotNull() {
+		assertNotNull(cdPlayer);
+	}
+	
+	@Test
+	public void testCDPlayerPlay() {
+		cdPlayer.play();
+		assertEquals("playing 붕붕 by 김하온", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
+	
+}
